0df1a193bcfd4547b17cb0048f537759132f7df0
Reginald Daniel <redaniel@valdosta.edu>
2013-02-05 11:03:13 -0500

    Finished play time at end of Section D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @cart = current_cart
+    @counter = increment_counter
+    @message = "You have visited here #{@counter} times" if session[:counter] > 5 
+  end
+  
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
   end
 end
